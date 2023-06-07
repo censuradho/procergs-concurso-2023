82539a897f7f52ce13cd432012ac38a3eb032c05
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-06-07 16:13:10 -0300

    comment api call

--- src/app/(procergs)/page.tsx
+++ src/app/(procergs)/page.tsx
@@ -4,21 +4,21 @@ import { Table } from './components'
 import styles from './styles.module.css'
 
 export default async function Page  () {
-  const { data } = await findAll()
+  // const { data } = await findAll()
 
-  const sortedByRules = sortRules(data)
+  // const sortedByRules = sortRules(data)
 
-  const dataAddClassification = sortedByRules.map((value, index) => ({
-    ...value,
-    classification: index + 1
-  }))
+  // const dataAddClassification = sortedByRules.map((value, index) => ({
+  //   ...value,
+  //   classification: index + 1
+  // }))
 
   return (
     <main className={styles.home}>
       <div className="container">
         <h1 className={styles.home__title}>PROCERGS Concurso 2023</h1>
         <strong className={styles.home__subtitle}>Analista em Computação / ênfase em Desenvolvimento Front-End</strong>
-        <Table data={dataAddClassification} />
+        {/* <Table data={dataAddClassification} /> */}
       </div>
     </main>
   )
