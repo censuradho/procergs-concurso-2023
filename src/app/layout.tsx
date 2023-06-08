b309cfbd57cab99ce36ac8360f62a6a890e3531b
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-06-08 11:04:20 -0300

    feat(analytics)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,21 @@
+import { appSettings, isDevMode } from '@/config/app';
 import './globals.css'
+import ReactGA from "react-ga4";
+import { GoogleAnalytics } from '@/components';
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
+  if (appSettings.analytics) {
+    ReactGA.initialize(appSettings.analytics, { testMode: isDevMode })
+  }
+
   return (
     <html lang="pt-BR">
+      <GoogleAnalytics />
       <body>{children}</body>
     </html>
   )
